2feb3a0946dc97a8c855dc8db3b845cab3289e26
saif120991 <140159595+saif120991@users.noreply.github.com>
2023-08-30 07:27:53 +0000

    variables basics

---
+++ javascript_basics/variables.js
@@ -0,0 +1,7 @@
+const accountName = "saif ali khan";
+let accountEmail = "saif@gmail.com";
+var accountCountry = "india";
+accountState = "delhi";
+
+// console.log(accountName);
+console.table([accountName,accountEmail,accountCountry,accountState]);
